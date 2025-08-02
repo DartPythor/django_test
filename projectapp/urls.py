7abde33bb57e761ed5194ee23fcdca2d98bb13bf
DarthByte <neverovm28@gmail.com>
2025-08-02 19:14:33 +0400

    fix: Убрад слеши для путей эндпоинтов

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -6,5 +6,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("vehicle", include("vehicle.urls", namespace="vehicle")),
+    path("vehicle/", include("vehicle.urls", namespace="vehicle")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
