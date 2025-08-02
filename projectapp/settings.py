78f6897b32b9139547d72b35fa08623546ae405f
DarthByte <neverovm28@gmail.com>
2025-08-02 17:58:36 +0400

    feat: Изменил настройки для создания типа техники и работы с статикой

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "vehicle.apps.VehicleConfig",
 ]
 
 MIDDLEWARE = [
@@ -41,7 +42,7 @@ ROOT_URLCONF = "projectapp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -90,5 +91,15 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = "media/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR /  "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
