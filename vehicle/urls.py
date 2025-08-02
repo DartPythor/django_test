f395e8e87ee0de8c0160612802b07c7e7a7cb6e6
DarthByte <neverovm28@gmail.com>
2025-08-02 19:15:40 +0400

    feat: Добавил эндпоинт/шаблон/путь для изменения типа техники

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from vehicle.views import VehicleTypeCreateView
+from vehicle.views import VehicleTypeCreateView, VehicleTypeUpdateView
 
 app_name = "vehicle"
 urlpatterns = [
     path(
-        "/vehicle-types/create/",
+        "vehicle-types/create",
         VehicleTypeCreateView.as_view(),
         name="vehicle_type_create",
+    ),
+    path(
+        "vehicle-types/<int:pk>",
+        VehicleTypeUpdateView.as_view(),
+        name="vehicle_type_read_update",
     ),
 ]
--- vehicle/views.py
+++ vehicle/views.py
@@ -1,9 +1,19 @@
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy, reverse
 from vehicle.models import VehicleType
-
 
 class VehicleTypeCreateView(CreateView):
     template_name = "vehicle/vehicletype_form.html"
     model = VehicleType
     fields = ["name"]
+    success_url = reverse_lazy("vehicle:vehicle_type_create")
+
+
+class VehicleTypeUpdateView(UpdateView):
+    template_name = "vehicle/vehicletype_edit_form.html"
+    model = VehicleType
+    fields = ["name"]
+    success_url = reverse_lazy("vehicle_type_list")
+
+    def get_success_url(self):
+        return reverse("vehicle:vehicle_type_read_update", args=(self.object.pk,))
