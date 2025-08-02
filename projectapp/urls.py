c3af875e43a7c0a300022ed76150ed5ca5baa4b4
DarthByte <neverovm28@gmail.com>
2025-08-02 23:35:31 +0400

    feat: Добавил статику и медиа при дебаг моде

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -7,4 +7,8 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("vehicle/", include("vehicle.urls", namespace="vehicle")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
